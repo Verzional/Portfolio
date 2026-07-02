e80592659f1431f6a8fb87ddcf1cedc0de3822bb
Verzional <verzional777@gmail.com>
2026-07-02 15:01:15 +0700

    chore(assets): add edo-sz font and image assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/views/hero";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-dvh bg-gray-900">
-      <p className="text-white">Home Page</p>
+    <div className="flex flex-col items-center justify-center min-h-dvh">
+      <Hero />
     </div>
   );
 }
